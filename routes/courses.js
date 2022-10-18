0a7d54d97518b2297ac9915bba43c47f9ce514a8
Jonas Roels <roels.jonas@protonmail.com>
2022-10-18 17:02:31 +0200

    added navbar, courses, about

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FlashCardz' });
+  res.render('courses', { title: 'FlashCardz' });
 });
 
 module.exports = router;
