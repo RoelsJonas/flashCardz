6329e461006ec10d53e802fbd943fb760e7efc9e
Bossch <76837096+Hunter8Dark@users.noreply.github.com>
2022-11-13 14:59:11 +0100

    Deleted tutorial

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 // });
 // GET home page.
 router.get("/", function (req, res) {
-  res.redirect("/catalog");
+  res.render("home", { title: 'Flashcards'});
 });
 
 module.exports = router;
