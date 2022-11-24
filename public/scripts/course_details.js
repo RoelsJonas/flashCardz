dd02c37286a6a9c616898a6b49863fbedc93e512
Jonas Roels <roels.jonas@protonmail.com>
2022-11-24 20:50:17 +0100

    added link sharing

--- public/scripts/course_details.js
+++ public/scripts/course_details.js
@@ -1,7 +1,15 @@
-const shareButton = document.getElementById("btn-share");
+const shareQrButton = document.getElementById("btn-share-qr");
+const shareLinkButton = document.getElementById("btn-share-link");
 const shareDiv = document.getElementById("share");
+const courseName = document.getElementById("course-name").innerText;
+const copiedLabel = document.getElementById("lbl-copied");
 
-shareButton.addEventListener("click", () => {
-    shareButton.remove();
+shareQrButton.addEventListener("click", () => {
     shareDiv.innerHTML = '<img src="https://qrtag.net/api/qr_transparent_12.svg?url=' + window.location.href + '"" alt="qrCode">';
+});
+
+shareLinkButton.addEventListener('click', () => {
+    navigator.clipboard.writeText("Start studying " + courseName + " now at: " + window.location.href)
+    copiedLabel.style = "scale: 1";
+    copiedLabel.style = "transition: opacity .5s ease-in 2s; opacity: 0;";
 });
