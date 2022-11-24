c449ed4ac03434f1225751fdfb7234d3272fa7f9
Jonas Roels <roels.jonas@protonmail.com>
2022-11-24 20:32:09 +0100

    fixed css for qr code

--- public/scripts/course_details.js
+++ public/scripts/course_details.js
@@ -2,5 +2,6 @@ const shareButton = document.getElementById("btn-share");
 const shareDiv = document.getElementById("share");
 
 shareButton.addEventListener("click", () => {
+    shareButton.remove();
     shareDiv.innerHTML = '<img src="https://qrtag.net/api/qr_transparent_12.svg?url=' + window.location.href + '"" alt="qrCode">';
 });
