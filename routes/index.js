35055a40b442743339346f4e2c3dc0ce25b4a10b
Jonas Roels <roels.jonas@protonmail.com>
2022-12-04 14:06:11 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,6 @@ var router = express.Router();
 var {isLoggedIn, state} = require("../controllers/middleware"); 
 
 router.get("/", isLoggedIn, function (req, res) {
-  if(req.headers.host != "localhost:3000") {
-    if(req.protocol==='http') {
-      console.log("redirecting");
-      res.redirect("https://" + req.headers.host + req.url);
-      return;
-    }
-  }
   var user = undefined;
   if(req.user) user = req.user;
   res.render("home", { title: 'Flashcards', user: user, page: "dashboard"});
